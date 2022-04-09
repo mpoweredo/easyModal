2f00a9106676ccf9e54c28d1e69d911e2c40c48e
mpoweredo <mpow3r1337@gmail.com>
2022-04-09 12:40:02 +0200

    something

--- src/scripts/App.js
+++ src/scripts/App.js
@@ -1,14 +1,15 @@
 import { EasyModal } from "./easyModal.js";
 
 new EasyModal({
-	title: "Kalifornia - USA",
+	title: "USA",
 	description:
-		"Kalifornia to najwiekszy stan w USA, zamieszkuje go ponad 40 mln osob",
-	theme: "light",
-	operationButton: [
-		{ content: "Wykup lot", href: "airbnb.com" },
-		{ content: "Zaloguj sie", href: "jd.pl" }
-	],
+		"The United States is one of the largest countries in the world - in terms of area it is only ahead of Russia, Canada and China. There are also densely populated cities, among which New York is the leader, with over 8 million people.",
+	theme: "dark",
+	// operationButton: [
+	// 	{ content: "Wykup lot", href: "airbnb.com" },
+	// 	{ content: "Zaloguj sie", href: "jd.pl" }
+	// ],
 });
 
 // new EasyModal;
+
